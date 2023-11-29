664aaa1b559ddb8943934a61efd700d2dd8d73f8
Amal717 <thomasamal43@gmail.com>
2023-11-29 22:59:42 +0530

    added new template

--- individual/individual.js
+++ individual/individual.js
@@ -1,39 +1,67 @@
-function selectTemplate(templateNumber) {
-    // Hide template options and show the form
-    document.getElementById('template-options').style.display = 'none';
-    document.getElementById('form-section').style.display = 'block';
+let templateImage; // Store the selected template image globally
 
+function selectTemplate(templateNumber) {
     // Create a new image element for the selected template
-    const selectedTemplateContainer = document.getElementById('selected-template-container');
-    const selectedTemplateImg = document.createElement('img');
+    const selectedTemplateImg = new Image();
     selectedTemplateImg.src = `${templateNumber}.jpg`;
     selectedTemplateImg.alt = `Template ${templateNumber}`;
     selectedTemplateImg.style.width = '250px'; // Set a specific width for the selected template image
     selectedTemplateImg.style.height = 'auto'; // Maintain aspect ratio
-    
+
     // Clear any existing content in the container and append the selected template image
+    const selectedTemplateContainer = document.getElementById('selected-template-container');
     selectedTemplateContainer.innerHTML = '';
     selectedTemplateContainer.appendChild(selectedTemplateImg);
+
+    // Store the selected template image globally
+    templateImage = selectedTemplateImg;
 }
 
 function generateCertificate() {
+    if (!templateImage) {
+        console.error('No template selected');
+        return;
+    }
+
     // Retrieve form data
     const participantName = document.getElementById('participant-name').value;
     const institutionName = document.getElementById('institution-name').value;
     const eventName = document.getElementById('event-name').value;
 
-    // Create a new PDF document
-    const pdf = new jsPDF();
+    // Define the x, y coordinates for placing text (replace these values with your actual coordinates)
+    const participantNameX = 397;
+    const participantNameY = 308;
+    const institutionNameX = 100;
+    const institutionNameY = 250;
+    const eventNameX = 100;
+    const eventNameY = 300;
 
-    // Set font size and style
-    pdf.setFontSize(16);
-    pdf.setFont('helvetica', 'normal');
+    // Create a new canvas element
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
 
-    // Add content to the PDF
-    pdf.text(`Certificate for ${participantName}`, 20, 30);
-    pdf.text(`Institution: ${institutionName}`, 20, 50);
-    pdf.text(`Event: ${eventName}`, 20, 70);
+    // Set canvas size to the original size of the template image
+    canvas.width = templateImage.naturalWidth;
+    canvas.height = templateImage.naturalHeight;
+    
 
-    // Save the PDF with a specific name
-    pdf.save('certificate.pdf');
+    // Draw the template image on the canvas
+    context.drawImage(templateImage, 0, 0, templateImage.naturalWidth, templateImage.naturalHeight);
+
+    // Add text to the canvas at specific coordinates
+    context.fillStyle = 'black'; // Set text color to black
+    context.font = 'bold 16px Arial';
+
+    // Add participant name
+    context.fillText(participantName, participantNameX, participantNameY);
+
+    // Add institution name
+    context.fillText(institutionName, institutionNameX, institutionNameY);
+
+    // Add event name
+    context.fillText(eventName, eventNameX, eventNameY);
+
+    // Show the generated certificate in a new window
+    const newWindow = window.open();
+    newWindow.document.body.appendChild(canvas);
 }
