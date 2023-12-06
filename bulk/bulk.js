70335c6fd026b3c639fdea3a0ed2d6dd49a9abbd
Amal717 <64456630+Amal717@users.noreply.github.com>
2023-12-06 23:33:52 +0530

    Update bulk.js

--- bulk/bulk.js
+++ bulk/bulk.js
@@ -14,49 +14,102 @@ function selectTemplate(templateNumber) {
     templateImage = selectedTemplateImg;
 }
 
-function processCSV(csvData) {
-    const lines = csvData.split('\n');
-
-    for (let i = 1; i < lines.length; i++) {
-        const participantName = lines[i].trim();
-        if (participantName !== '') {
-            // Send data to backend for processing
-            sendToBackend(participantName);
-        }
+function generateCertificate(participantName) {
+    if (!templateImage) {
+        console.error('No template selected');
+        return;
     }
-}
 
-function sendToBackend(participantName) {
     // Get other form data
     const institutionName = document.getElementById('institution-name').value;
     const eventName = document.getElementById('event-name').value;
 
-    // Prepare data to be sent to the backend
-    const data = {
-        participantName: participantName,
-        institutionName: institutionName,
-        eventName: eventName,
-        templateSrc: templateImage.src,
-    };
+    // Define the x, y coordinates for placing text (replace these values with your actual coordinates)
+    const participantNameX = 682;
+    const participantNameY = 707;
+    const institutionNameX = 368;
+    const institutionNameY = 883;
+    const eventNameX = 1012;
+    const eventNameY = 984;
 
-    // Send data to backend using fetch
-    fetch('/process_image', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(data),
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Backend response:', data);
-    })
-    .catch(error => {
-        console.error('Error sending data to backend:', error);
-    });
+    // Create a new canvas element
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
+
+    // Set canvas size to the original size of the template image
+    canvas.width = templateImage.naturalWidth;
+    canvas.height = templateImage.naturalHeight;
+
+    // Draw the template image on the canvas
+    context.drawImage(templateImage, 0, 0, templateImage.naturalWidth, templateImage.naturalHeight);
+
+    // Add text to the canvas at specific coordinates
+    context.fillStyle = 'black'; // Set text color to black
+    context.font = 'bold 50px Arial';
+
+    // Add participant name
+    context.fillText(participantName, participantNameX, participantNameY);
+
+    // Add institution name
+    context.fillText(institutionName, institutionNameX, institutionNameY);
+
+    // Add event name
+    context.fillText(eventName, eventNameX, eventNameY);
+
+    // Convert the canvas to a data URL representing a PNG
+    const imageDataURL = canvas.toDataURL('image/png');
+
+    return { participantName, imageDataURL };
 }
 
-function uploadCSV() {
+function compressCertificates(certificates) {
+    // Use JSZip library to create a zip file
+    const zip = new JSZip();
+
+    certificates.forEach(({ participantName, imageDataURL }) => {
+        const folder = zip.folder(participantName);
+        folder.file(`${participantName}_certificate.png`, imageDataURL.split(';base64,')[1], { base64: true });
+    });
+
+    // Generate a blob from the zip
+    return zip.generateAsync({ type: 'blob' })
+        .then(blob => {
+            // Create a download link for the zip file
+            const downloadLink = document.createElement('a');
+            downloadLink.href = URL.createObjectURL(blob);
+            downloadLink.download = 'certificates.zip';
+            document.body.appendChild(downloadLink);
+
+            // Trigger a click event on the link to start the download
+            downloadLink.click();
+
+            // Remove the link from the DOM
+            document.body.removeChild(downloadLink);
+        })
+        .catch(error => {
+            console.error('Error generating zip file:', error);
+        });
+}
+
+function processCSV(csvData) {
+    const lines = csvData.split('\n');
+
+    const certificates = [];
+
+    for (let i = 1; i < lines.length; i++) {
+        const participantName = lines[i].trim();
+        if (participantName !== '') {
+            const certificate = generateCertificate(participantName);
+            certificates.push(certificate);
+        }
+    }
+
+    // After processing all certificates, compress them into a zip file
+    compressCertificates(certificates);
+}
+
+function generateAndDownloadCertificates() {
+    // Get the participant name from the CSV file
     const input = document.getElementById('csv-file');
     const file = input.files[0];
 
@@ -79,8 +132,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const generateButton = document.getElementById('generate-button');
     if (generateButton) {
         generateButton.addEventListener('click', function () {
-            // This button click triggers the CSV processing and sending data to the backend
-            uploadCSV();
+            // This button click triggers the CSV processing and downloading of the zip file
+            generateAndDownloadCertificates();
         });
     }
 });
